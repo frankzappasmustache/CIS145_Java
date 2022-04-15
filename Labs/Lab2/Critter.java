94de53fdd11a41e7c6ca5e09f00080f04d57135d
frankzappasmustache <dustin.t.mcclure@gmail.com>
2022-04-15 12:11:44 -0700

    adding Lab 2 Critter

---
+++ Labs/Lab2/Critter.java
@@ -0,0 +1,20 @@
+/*
+    Copyright 2022 <Dustin McClure>
+    Project Name: CS145_Java
+    Sub-project: Lab 2
+    File Name: Critter.java
+    Author: Dustin McClure
+    Lab: Critter
+    Modified Date: 04/15/2022
+
+    This program
+*/
+import java.util.*;
+public class Critter {
+    public static void main(String[] args) {
+        CritterFrame = new CritterFrame
+    }
+
+}
+
+public class
