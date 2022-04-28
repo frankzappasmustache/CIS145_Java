f75fb65eebc98b81cb30338e2ee3e0b1f3fd65fb
frankzappasmustache <dustin.t.mcclure@gmail.com>
2022-04-27 23:46:24 -0700

    pushing unfinished card game lab

--- Labs/Lab3/cardGame/DeckOfCardsTest.java
+++ Labs/Lab3/cardGame/DeckOfCardsTest.java
@@ -83,12 +83,5 @@ public class DeckOfCardsTest {
             }
             answer = myDeckOfCards.menu(input);
         }
-        for(int i = 1; i <= 52; i++) {
-            System.out.printf("%-19s", myDeckOfCards.dealCard());
-
-            if(i % 4 == 0) {
-                System.out.println();
-            }
-        }
     }
 }
